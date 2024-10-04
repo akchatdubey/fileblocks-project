190c0cdcc984e5fe3b2fb5dacb7f7f7e8ae42054
akchat dubey <dubeyakchat50@gmail.com>
2024-10-04 14:46:55 +0530

    practice

---
+++ 24CAH606/xyz.py
@@ -0,0 +1,7 @@
+def calculate_average(numbers):
+    total = sum(numbers)
+    average = total / len(numbers)
+    print("The average is:", average)
+
+numbers = [5, 7, 9, 12, 15]
+calculate_average(numbers)
